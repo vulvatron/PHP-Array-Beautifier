f34abd8e9b97ce104c885e9c041e018fec31aa17
Olivier Lemire <oli.lemire@gmail.com>
2013-09-16 16:03:20 -0400

    HTML cleanup + Added get/print methods
    Added a way to get the data as string or be printed directly.
    Comments were modified to be compatible with phpDocumentor.
    HTML was stripped and fixed to be stand-alone, there were still references to the original CMS.

--- tool_php_array_beautifier.php
+++ tool_php_array_beautifier.php
@@ -1,17 +1,15 @@
 <?php
 /*
-Template Name: Tool - PHP Array Beautifier
-
-    Author:         Phillihp Harmon
-    Date:           2012.01.04
-    Contributor(s):     
-    
-    Description:    Copy a print_r() or var_dump() string into the beautifier and clean it up, indent,
-                    and add color codings for easy readability.
-*/
-?>
-
-<?php
+ * Tool - PHP Array Beautifier 
+ *
+ * Copy a print_r() or var_dump() string into the beautifier and clean it up, indent,
+ * and add color codings for easy readability.
+ *   
+ * @author          Phil Harmon
+ * @contributors    Olivier Lemire
+ * @date            2013.09.16
+ *
+ */
 
 class Debug {
     var $indent_size;
@@ -23,29 +21,35 @@ class Debug {
         "MidnightBlue",
         "FireBrick",
         "DarkGreen"
-        );
-    
-    function __construct() {
-        $this->indent_size = '20';
-    }
-    
+    );
+
     /*
-    *   Author: Phil Harmon
-    *   Description:
-    *       Take an array and format it to style in HTML
-    *   
-    *   Tasks:
-    *       - Add automated color formatting                    
-    */    
-    function array_to_html($val) {
+     * __construct
+     *
+     */
+    public function __construct($ident=20) {
+        $this->indent_size = $ident;
+    }
+
+    /*
+     * _process 
+     *
+     * Internal function that takes an array and format it to style in HTML.
+     *   
+     * @author          Phil Harmon
+     * @contributors    Olivier Lemire
+     * @access      private
+     * @param       mixed   string  Data as string
+     *                      array   Data ar real array
+     * @return      null
+     *
+     */
+    private function _process($val) {
         $do_nothing = true;
         
         // Get string structure
         if(is_array($val)) {
-            ob_start();
-            print_r($val);
-            $val = ob_get_contents();
-            ob_end_clean();
+            $val = print_r($val, true);
         }
         
         // Color counter
@@ -53,6 +57,7 @@ class Debug {
         
         // Split the string into character array
         $array = preg_split('//', $val, -1, PREG_SPLIT_NO_EMPTY);
+
         foreach($array as $char) {
             if($char == "[")
                 if(!$do_nothing)
@@ -74,34 +79,75 @@ class Debug {
             }
         }
     }
+
+    /*
+     * get_html 
+     *
+     * Public function that takes input array and output the formated HTML
+     *   
+     * @author      Olivier Lemire
+     * @access      public
+     * @param       mixed   string  Data as string
+     *                      array   Data ar real array
+     * @return      string  Formated
+     *
+     */
+    public function get_html($val) {
+        ob_start();
+        $this->_process($val);
+        $output = ob_get_contents();
+        ob_end_clean();
+        return $output;
+    }
+
+    /*
+     * print_html 
+     *
+     * Public function that takes input array and print it directly to the user screen
+     *   
+     * @author      Olivier Lemire
+     * @access      public
+     * @param       mixed   string  string array
+     *                      array   php array
+     * @return      null
+     *
+     */
+    public function print_html($val) {
+        $this->_process($val);
+    }
 }
 
-$postdata = g('to_convert');
-get_header();
-?>
-
-<div id="container" class="one-column">
-	<div id="content" role="main">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-        <div>This simple tool takes an array or object output in PHP, such as a print_r() statement and formats it in color coding to easily read your data.</div><br />
-<?php if($postdata): ?>
-<div><a href="http://phillihp.com/toolz/php-array-beautifier/">Go back</a></div>
-<div style="width: 940px; overflow: auto;">
-<?php
+if ($postdata = $_POST['to_convert']) {
     $d = new Debug();
-    $d->array_to_html($postdata);
-?>
-</div>
-<?php endif; ?>
+}
+?><!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <title>PHP-Array-Beautifier</title>
+</head>
+<body>
+    <article>
+        <header>
+            <h1>PHP-Array-Beautifier</h1>
+        </header>
+        <div class="content">
+            <p>This simple tool takes an array or object output in PHP, such as a print_r() statement and formats it in color coding to easily read your data.</div><br />
 
-<form action="http://phillihp.com/toolz/php-array-beautifier/" method="POST">
-    <div>Code to Transform:</div>
-    <div><textarea id='to_conv' name='to_convert' rows='14' cols='120'><?php echo $postdata; ?></textarea></div>
-    <div><input type='submit' value='Beautify' /> <span onClick="document.getElementById('to_conv').value = 'Array([mode] => sets[sets] => Array([0] => 123[1] => 456[2] => 789)[etc] => Array([letters] => Array([0] => a[1] => b)[0] => pie[1] => sharks))';" style="color: blue; cursor: pointer;">Example</a></div>
-</form>
+            <form action="<?php echo basename(__FILE__); ?>" method="POST">
+                <div>Code to Transform:</div>
+                <div><textarea id='to_conv' name='to_convert' rows='14' cols='120'><?php echo $postdata; ?></textarea></div>
+                <div><input type='submit' value='Beautify' /> <span onClick="document.getElementById('to_conv').value = 'Array([mode] => sets[sets] => Array([0] => 123[1] => 456[2] => 789)[etc] => Array([letters] => Array([0] => a[1] => b)[0] => pie[1] => sharks))';" style="color: blue; cursor: pointer;">Example</a></div>
+            </form>
 
-<?php comments_template( '', true ); ?>
-	</div><!-- #content -->
-</div><!-- #container -->
-
-<?php get_footer(); ?>
+            <?php if ($postdata): ?>
+                <div><a href="<?php echo basename(__FILE__); ?>">Go back</a></div>
+                <div style="width: 940px; overflow: auto;"><?php $d->print_html($postdata); ?></div>
+            <?php endif; ?>
+	   </div>
+       <footer>
+        <p>Based on <a href="http://phillihp.com/toolz/php-array-beautifier/">Phillihp Harmon</a>'s script</p>
+       </footer>
+    </article>
+</body>
+</html>
